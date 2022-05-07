4c4667712d022dc9c066e8407af029041d66d02a
creativeapps <creativeapps2020@gmail.com>
2022-05-07 14:19:25 +0200

    add Firebase AppCheck for android

--- drosak/lib/main.dart
+++ drosak/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 import 'firebase_options.dart';
 
@@ -8,6 +9,10 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  );
+
   runApp(const MyApp());
 }
 
