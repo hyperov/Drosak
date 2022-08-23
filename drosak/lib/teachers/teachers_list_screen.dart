ef98a72a1a1ff53c88fba578b08d58986eea047a
creativeapps <creativeapps2020@gmail.com>
2022-08-24 01:24:08 +0200

    make teachers list screen refreshable

--- drosak/lib/teachers/teachers_list_screen.dart
+++ drosak/lib/teachers/teachers_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:drosak/common/model/empty_widget.dart';
+import 'package:drosak/common/viewmodel/filter_viewmodel.dart';
 import 'package:drosak/extensions/teacher_extensions.dart';
 import 'package:drosak/teachers/viewmodel/teachers_list_viewmodel.dart';
 import 'package:drosak/utils/localization/localization_keys.dart';
@@ -16,6 +17,8 @@ class TeachersListScreen extends StatelessWidget {
   TeachersListScreen({Key? key}) : super(key: key);
 
   TeachersListViewModel get _teachersListViewModel => Get.find();
+
+  FilterViewModel get _filterViewModel => Get.find();
   final _storage = GetStorage();
 
   @override
@@ -25,52 +28,60 @@ class TeachersListScreen extends StatelessWidget {
         body: _teachersListViewModel.isLoading.value
             ? const Center(child: CircularProgressIndicator())
             : _teachersListViewModel.teachersList.isNotEmpty
-                ? ListView.builder(
-                        itemBuilder: (context, index) {
-                          return Card(
-                            elevation: 4,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: InkWell(
-                              onTap: () async {
-                                _teachersListViewModel.selectedTeacher =
-                                    _teachersListViewModel.teachersList[index];
-                                await _storage.write(StorageKeys.teacherId,
-                                    _teachersListViewModel.selectedTeacher.id);
-                                await _storage.write(
-                                    StorageKeys.teacherRating,
-                                    _teachersListViewModel
-                                        .selectedTeacher.avgRating);
-                                Get.to(() => TeacherDetailsScreen());
-                              },
-                              child: Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 24),
-                                child: Stack(
-                                  children: [
-                                    Row(
+                ? RefreshIndicator(
+                    onRefresh: _refresh,
+                    color: ColorManager.redOrangeLight,
+                    backgroundColor: ColorManager.blueDark,
+                    displacement: 50,
+                    child: ListView.builder(
+                            itemBuilder: (context, index) {
+                              return Card(
+                                elevation: 4,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: InkWell(
+                                  onTap: () async {
+                                    _teachersListViewModel.selectedTeacher =
+                                        _teachersListViewModel
+                                            .teachersList[index];
+                                    await _storage.write(
+                                        StorageKeys.teacherId,
+                                        _teachersListViewModel
+                                            .selectedTeacher.id);
+                                    await _storage.write(
+                                        StorageKeys.teacherRating,
+                                        _teachersListViewModel
+                                            .selectedTeacher.avgRating);
+                                    Get.to(() => TeacherDetailsScreen());
+                                  },
+                                  child: Container(
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 16, vertical: 24),
+                                    child: Stack(
                                       children: [
-                                        Stack(
-                                          alignment: Alignment.center,
-                                          clipBehavior: Clip.none,
+                                        Row(
                                           children: [
-                                            Hero(
-                                              tag: _teachersListViewModel
-                                                  .teachersList[index].id!,
-                                              child: Material(
-                                                color: Colors.transparent,
-                                                child: Card(
-                                                  color: Colors.deepPurple,
-                                                  elevation: 4,
-                                                  clipBehavior: Clip.hardEdge,
-                                                  shape: const CircleBorder(
-                                                      side: BorderSide(
-                                                          color: Colors
-                                                              .deepPurpleAccent,
-                                                          width: 1)),
-                                                  child: Obx(() =>
-                                                      _teachersListViewModel
+                                            Stack(
+                                              alignment: Alignment.center,
+                                              clipBehavior: Clip.none,
+                                              children: [
+                                                Hero(
+                                                  tag: _teachersListViewModel
+                                                      .teachersList[index].id!,
+                                                  child: Material(
+                                                    color: Colors.transparent,
+                                                    child: Card(
+                                                      color: Colors.deepPurple,
+                                                      elevation: 4,
+                                                      clipBehavior:
+                                                          Clip.hardEdge,
+                                                      shape: const CircleBorder(
+                                                          side: BorderSide(
+                                                              color: Colors
+                                                                  .deepPurpleAccent,
+                                                              width: 1)),
+                                                      child: Obx(() => _teachersListViewModel
                                                               .teachersList[
                                                                   index]
                                                               .photoUrl
@@ -101,99 +112,112 @@ class TeachersListScreen extends StatelessWidget {
                                                                 height: 70,
                                                               );
                                                             })),
+                                                    ),
+                                                  ),
                                                 ),
-                                              ),
-                                            ),
-                                            Positioned(
-                                              child: Container(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
+                                                Positioned(
+                                                  child: Container(
+                                                    padding: const EdgeInsets
+                                                            .symmetric(
                                                         horizontal: 8,
                                                         vertical: 2),
-                                                decoration: BoxDecoration(
-                                                  color: Colors.white,
-                                                  border: Border.all(
-                                                      color: Colors
-                                                          .deepPurpleAccent),
-                                                  borderRadius: const BorderRadius
-                                                          .horizontal(
-                                                      left: Radius.circular(16),
-                                                      right:
-                                                          Radius.circular(16)),
-                                                ),
-                                                child: Row(children: [
-                                                  SvgPicture.asset(
-                                                    AssetsManager.star,
-                                                    width: 16,
-                                                    height: 16,
+                                                    decoration: BoxDecoration(
+                                                      color: Colors.white,
+                                                      border: Border.all(
+                                                          color: Colors
+                                                              .deepPurpleAccent),
+                                                      borderRadius:
+                                                          const BorderRadius
+                                                                  .horizontal(
+                                                              left: Radius
+                                                                  .circular(16),
+                                                              right: Radius
+                                                                  .circular(
+                                                                      16)),
+                                                    ),
+                                                    child: Row(children: [
+                                                      SvgPicture.asset(
+                                                        AssetsManager.star,
+                                                        width: 16,
+                                                        height: 16,
+                                                      ),
+                                                      const SizedBox(width: 4),
+                                                      Text(
+                                                          _teachersListViewModel
+                                                              .teachersList[
+                                                                  index]
+                                                              .avgRating
+                                                              .toString(),
+                                                          style:
+                                                              const TextStyle(
+                                                                  fontSize: 10,
+                                                                  color: Colors
+                                                                      .black)),
+                                                    ]),
                                                   ),
-                                                  const SizedBox(width: 4),
+                                                  bottom: -5,
+                                                ),
+                                              ],
+                                            ),
+                                            const SizedBox(width: 16),
+                                            Expanded(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
                                                   Text(
                                                       _teachersListViewModel
                                                           .teachersList[index]
-                                                          .avgRating
-                                                          .toString(),
+                                                          .name!,
                                                       style: const TextStyle(
-                                                          fontSize: 10,
-                                                          color: Colors.black)),
-                                                ]),
-                                              ),
-                                              bottom: -5,
-                                            ),
-                                          ],
-                                        ),
-                                        const SizedBox(width: 16),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                  _teachersListViewModel
+                                                          fontSize: 16,
+                                                          fontWeight:
+                                                              FontWeight.bold)),
+                                                  Text(_teachersListViewModel
                                                       .teachersList[index]
-                                                      .name!,
-                                                  style: const TextStyle(
-                                                      fontSize: 16,
-                                                      fontWeight:
-                                                          FontWeight.bold)),
-                                              Text(_teachersListViewModel
-                                                  .teachersList[index]
-                                                  .material!),
-                                              Text(_teachersListViewModel
-                                                  .teachersList[index]
-                                                  .getEducationText()),
-                                            ],
-                                          ),
-                                        ),
-                                        Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            Text(
-                                                '${_teachersListViewModel.teachersList[index].priceMin} - '
-                                                '${_teachersListViewModel.teachersList[index].priceMax} ج ',
-                                                style: const TextStyle(
-                                                    fontSize: 16,
-                                                    color:
-                                                        ColorManager.deepPurple,
-                                                    fontWeight:
-                                                        FontWeight.bold)),
-                                            Text(LocalizationKeys
-                                                .price_average.tr),
+                                                      .material!),
+                                                  Text(_teachersListViewModel
+                                                      .teachersList[index]
+                                                      .getEducationText()),
+                                                ],
+                                              ),
+                                            ),
+                                            Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                    '${_teachersListViewModel.teachersList[index].priceMin} - '
+                                                    '${_teachersListViewModel.teachersList[index].priceMax} ج ',
+                                                    style: const TextStyle(
+                                                        fontSize: 16,
+                                                        color: ColorManager
+                                                            .deepPurple,
+                                                        fontWeight:
+                                                            FontWeight.bold)),
+                                                Text(LocalizationKeys
+                                                    .price_average.tr),
+                                              ],
+                                            )
                                           ],
-                                        )
+                                        ),
                                       ],
                                     ),
-                                  ],
+                                  ),
                                 ),
-                              ),
-                            ),
-                          );
-                        },
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: _teachersListViewModel.teachersList.length)
-                    .paddingOnly(top: 20)
-                    .marginSymmetric(horizontal: 16)
+                              );
+                            },
+                            // physics: const BouncingScrollPhysics(),
+                            itemCount:
+                                _teachersListViewModel.teachersList.length)
+                        .paddingOnly(top: 20)
+                        .marginSymmetric(horizontal: 16),
+                  )
                 : EmptyView(title: LocalizationKeys.teachers_not_found.tr)));
+  }
+
+  Future<void> _refresh() async {
+    await _filterViewModel.applyFilter();
+    return Future.delayed(const Duration(seconds: 0));
   }
 }
